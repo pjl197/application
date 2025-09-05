4777bc73a47096b4db3f0ee092deb452bb4cf5c3
彭金龙 <1973935855@qq.com>
2025-09-05 17:13:31 +0800

    新增demo

--- springbootquickstarter/src/main/java/com/example/controller/DemoController.java
+++ springbootquickstarter/src/main/java/com/example/controller/DemoController.java
@@ -1,4 +1,10 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class DemoController {
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
 }
